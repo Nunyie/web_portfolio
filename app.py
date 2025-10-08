bd93628e72637b05de407759dfb32c085ae1bd7f
Nunyie <roswellbunag05@gmail.com>
2025-10-08 14:48:26 +0800

    Created an intro and a homepage, with buttons for ui navigation

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
+def intro():
+    return render_template('intro.html')
+
+@app.route('/homepage')
 def index():
-    return render_template('index.html')
+    return render_template('homepage.html')
 
 @app.route('/profile')
 def profile():
@@ -16,7 +20,16 @@ def works():
     if request.method == 'POST':
         input_string = request.form.get('inputString', '')
         result = input_string.upper()
+    return render_template('works.html', result=result)
+
+@app.route('/works/toUpperCase', methods=['GET', 'POST'])
+def touppercase():
+    result = None
+    if request.method == 'POST':
+        input_string = request.form.get('inputString', '')
+        result = input_string.upper()
     return render_template('touppercase.html', result=result)
+
 
 @app.route('/works/area/circle', methods=['GET', 'POST'])
 def acircle():
@@ -25,6 +38,15 @@ def acircle():
         radius = request.form.get('radius', '')
         result = int(radius)*3.14*int(radius)
     return render_template('circle.html', result=result)
+
+@app.route('/works/area/triangle', methods=['GET', 'POST'])
+def atriangle():
+    result = None
+    if request.method == 'POST':
+        width = request.form.get('width', '')
+        height = request.form.get('height', '')
+        result = (int(width)*int(height))*.5
+    return render_template('triangle.html', result=result)
 
 @app.route('/contact')
 def contact():
